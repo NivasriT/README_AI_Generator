00228b89083d12fcdba1674442ede22587dec9fb
Nivasri Thirumeni <nivasrithirumeni24@gmail.com>
2026-07-03 18:44:23 +0530

    Update __init__.py

--- README_generator/services/__init__.py
+++ README_generator/services/__init__.py
@@ -6,6 +6,6 @@ Contains business logic and service layer components
 from . import zip_service
 from . import parser_service
 from . import prompt_service
-from . import gemini_service
+from . import groq_service
 
 __all__ = ['zip_service', 'parser_service', 'prompt_service', 'gemini_service']
